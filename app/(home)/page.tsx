2fd77b0e3ae8fb0541b4210ef9707fb3f4f33e1d
joseuilton <joseuilton.siqueira@gmai.com>
2024-03-25 17:59:53 -0300

    feat: add poll list on home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,21 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Button } from "../components/Button";
 import Link from "next/link";
 
+type Poll = {
+  id: string;
+  title: string;
+}
+
 export default function Home() {
-  const [polls, setPolls] = useState([]);
+  const polls = useMemo<Poll[]>(() => {
+    if (localStorage.getItem("@App:polls")) {
+      return JSON.parse(localStorage.getItem("@App:polls")!);
+    }
+
+    return null;
+  }, []);
 
   return (
     <main className="mt-16 flex flex-col justify-center items-center">
@@ -12,18 +23,27 @@ export default function Home() {
         Bem-vindo ao Polls App 🤩
       </h1>
 
-      <ul className="mt-8 bg-neutral-800 py-4 px-2 rounded-2xl lg:w-1/2 lg:p-6">
-        <h2 className="font-bold mb-4">Suas enquetes</h2>
+      {polls && (
+        <ul className="mt-8 bg-neutral-800 py-4 px-2 rounded-2xl lg:w-1/2 lg:p-6">
+          <h2 className="font-bold mb-4">Suas enquetes</h2>
 
-        <div className="flex flex-col gap-y-8">
-          <li>
-            <Link href="" className="block bg-neutral-900 rounded-lg px-4 py-2">
-              Qual é o melhor framework backend Javascript?
-            </Link>
-          </li>
-        </div>
+          <div className="flex flex-col gap-y-8">
+            {polls.map((poll) => (
+              <li key={poll.id}>
+                <Link
+                  href={`/polls/${poll.id}/results`}
+                  className="block bg-neutral-900 rounded-lg px-4 py-2"
+                >
+                  {poll.title}
+                </Link>
+              </li>
+            ))}
 
-      </ul>
+          </div>
+
+        </ul>
+      )}
+
 
       <h2 className="px-4 mt-8 text-lg text-center font-bold">
         Vamos criar uma nova enquete?
